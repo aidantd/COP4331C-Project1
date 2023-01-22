a2f36fb7fca6a20749bbbe89d3d2bd496105e8c3
Aidan Dion <aidantdion@gmail.com>
2023-01-22 11:55:23 -0500

    Create more functions and clean up a little

--- js/code.js
+++ js/code.js
@@ -1,16 +1,21 @@
-const urlBase = 'http://104.248.225.220/LAMPAPI';
+const urlBase = 'http://cop4331-5.xyz/LAMPAPI';
 const extension = 'php';
+
+let userId  = 0;
+let firstName = "";
+let lastName = "";
+
 
 function register() {
     firstName = document.getElementById("firstName").value;
     lastName = document.getElementById("lastName").value;
 
     let username = document.getElementById("newUserName").value;
-    let password = document.getElementById("newPassword");
+    let password = document.getElementById("newPassword").value;
 
     document.getElementById("registerStatus").innerHTML = "";
 
-    let tmp = {firstName: firstName, LastName: lastName, Username: username, Password: password};
+    let tmp = {FirstName: firstName, LastName: lastName, Username: username, Password: password};
     let jsonPayload = JSON.stringify(tmp);
 
     let url = urlBase + '/Register' + extension;
@@ -26,14 +31,88 @@ function register() {
                 document.getElementById("registerStatus").innerHTML = "Successfully Registered Please Login";
                 return;
             }
-        }
+        };
         xhr.send(jsonPayload);
     } 
     catch(err) {
-        document.getElementById("registerItem").innerHTM = "Failed To Register";
+        document.getElementById("registerItem").innerHTM = err.message;
     }
 }
 
 function login() {
-  
+    userId = 0;
+    let username = document.getElementById("usernameLogin").value;
+    let password = document.getElementById("passwordLogin").value;
+
+    let tmp = {Username: username, Password: password};
+    let jsonPayload = JSON.stringify( tmp );
+	
+	let url = urlBase + '/Login.' + extension;
+
+	let xhr = new XMLHttpRequest();
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+
+    try {
+		xhr.onreadystatechange = function() {
+			if (this.readyState == 4 && this.status == 200) {
+				let jsonObject = JSON.parse( xhr.responseText );
+				userId  = jsonObject.id;
+		
+				if(userId < 1) {		
+					document.getElementById("loginResult").innerHTML = "User/Password combination incorrect";
+					return;
+				}
+		
+				firstName = jsonObject.firstName;
+				lastName = jsonObject.lastName;
+
+				saveCookie();
+	
+				window.location.href = "home.html";
+			}
+		};
+		xhr.send(jsonPayload);
+	}
+	catch(err)
+	{
+		document.getElementById("loginResult").innerHTML = err.message;
+	}
+}
+
+function saveCookie()
+{
+	let minutes = 20;
+	let date = new Date();
+	date.setTime(date.getTime()+(minutes*60*1000));	
+	document.cookie = "Email=" + Email + ",userId =" + userId  + ";expires=" + date.toGMTString();
+}
+
+function readCookie()
+{
+	userId  = -1;
+	let data = document.cookie;
+	let splits = data.split(",");
+	for(var i = 0; i < splits.length; i++) 
+	{
+		let thisOne = splits[i].trim();
+		let tokens = thisOne.split("=");
+		if(tokens[0] == "Email")
+		{
+			Email = tokens[1];
+		}
+		else if(tokens[0] == "userId ")
+		{
+			userId  = parseInt( tokens[1].trim() );
+		}
+	}
+	
+	if(userId  < 0)
+	{
+		window.location.href = "home.html";
+	}
+	else
+	{
+		document.getElementById("Emaildisplay").innerHTML = Email;
+	}
 }
