e90f4ae211112729c160f0268db91fa9fa8a7f9e
AidanDion <55695921+AidanDion@users.noreply.github.com>
2023-01-24 14:49:59 -0500

    Create slider functionality on index page

--- js/code.js
+++ js/code.js
@@ -4,7 +4,20 @@ const extension = 'php';
 let userId  = 0;
 let firstName = "";
 let lastName = "";
-
+let signup = document.querySelector(".signup");
+let login = document.querySelector(".login");
+let slider = document.querySelector(".slider");
+let formSection = document.querySelector(".form-section");
+  
+signup.addEventListener("click", () => {
+    slider.classList.add("moveslider");
+    formSection.classList.add("form-section-move");
+});
+  
+login.addEventListener("click", () => {
+    slider.classList.remove("moveslider");
+    formSection.classList.remove("form-section-move");
+});
 
 function register() {
     firstName = document.getElementById("firstName").value;
