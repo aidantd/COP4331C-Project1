d7f691caed7d5c6977c14057c0c290f4e402413a
aidantd <aidantdion@gmail.com>
2023-01-27 15:57:54 -0500

    the funny

--- js/code.js
+++ js/code.js
@@ -174,7 +174,6 @@ function addContact() {
 	let lastNameAdd = document.getElementById("contact-info-name-last").value;
 	let phoneNumberAdd = document.getElementById("contact-info-email").value;
 	let emailAdd = document.getElementById("contact-info-phone").value;
-	let userId = userId;
 
 	let tmp = {firstName: firstNameAdd, lastName: lastNameAdd, phone: phoneNumberAdd, email: emailAdd, userID: userId};
 	let jsonPayload = JSON.stringify(tmp);
