d154d7237259141e652256f29105f0d020aa6e46
aidantd <aidantdion@gmail.com>
2023-02-09 16:05:17 -0500

    Add class

--- js/code.js
+++ js/code.js
@@ -151,7 +151,7 @@ function showAllContacts() {
 					"</td><td id='email[" + i + "]'>" + jsonObject.results[i].email + 
 					"</td><td>"
 					html += "<button type='button' class='btn btn-blue-navy' id='editButton[" + i + "]' onclick='editContact(" + i + ")'>Edit</button>";
-					html += "<button type='button' style='display: none' id='saveButton[" + i + "]' onclick='saveContact(" + i + "," + jsonObject.results[i].id + ")'>Save</button>"
+					html += "<button type='button' class='btn btn-blue-navy' style='display: none' id='saveButton[" + i + "]' onclick='saveContact(" + i + "," + jsonObject.results[i].id + ")'>Save</button>"
 					html += "<button type='button' class='btn btn-blue-navy' id='deleteButton[" + i + "]' onclick='deleteContact(" + i + ")'>Delete</button></td></tr></tbody>";
 				}
 				html += "</table></div></section>";
