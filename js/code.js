65a675e3ff66ed8c490b54eaf667955ab1306863
Kevin <vukevin0212@gmail.com>
2023-02-09 17:01:50 -0500

    contact table style

--- js/code.js
+++ js/code.js
@@ -142,7 +142,7 @@ function showAllContacts() {
 					document.getElementById("allContactInfo").innerHTML = html;
 					return;
 				}
-				html += "<section id=\"infoWindow\"><div class=\"container\"><table class=\"table\" id=\"contact-table\">"
+				html += "<section id=\"infoWindow\"><div class=\"container\"><table class=\"table contact-table\" id=\"contact-table\">"
 				html += "<thread><tr><th>First Name</th><th>Last Name</th><th>Phone Number</th><th>Email</th><th>Actions</th></tr></thread>";
 				for(let i = 0; i < jsonObject.results.length; i++) {
 					html += "<tbody><tr id='row[" + i + "]'><td id='firstName["  + i + "]'>" + jsonObject.results[i].firstName + 
